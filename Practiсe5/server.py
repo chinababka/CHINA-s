f60593f23ed34785621d72123a81b4b7272e43d3
chinababka <ruslanbyku@gmail.com>
2018-04-14 11:13:57 +0300

    add

---
+++ Practiсe5/server.py
@@ -0,0 +1,44 @@
+import socket, time
+
+host = socket.gethostbyname(socket.gethostname())
+port = 9090
+key = 3017
+
+clients = []
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind((host, port))
+
+quit = False
+print("[ Server Started ]")
+
+while not quit:
+    try:
+        data, addr = s.recvfrom(1024)
+
+        if addr not in clients:
+            clients.append(addr)
+
+        itsatime = time.strftime("%Y-%m-%d-%H.%M.%S", time.localtime())
+
+        print("[" + addr[0] + "]=[" + str(addr[1]) + "]=[" + itsatime + "]/", end="")
+        decrypt = ""
+        k = False
+        for i in data.decode("utf-8"):
+            if i == ":":
+                k = True
+                decrypt += i
+            elif k == False or i == " ":
+                decrypt += i
+            else:
+                decrypt += chr(ord(i) ^ key)
+        print(decrypt)
+
+        for client in clients:
+            if addr != client:
+                s.sendto(data, client)
+    except:
+        print("\n[ Server Stopped ]")
+        quit = True
+
+s.close()
